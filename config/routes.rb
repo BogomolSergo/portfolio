d50f72e6d027bd5cd9341c5196cd36b61b6ee8ce
bogomol_sergo <breinkiller650@gmail.com>
2020-01-28 00:05:39 +0200

    1 commit jan27 00:05

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+  get '/works' => 'works#index', as: :works
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+w1 = Work.create(title: 'Codeacademy', description: 'MOST MOST MOST', language: 'Ruby on Rails', price: 5000)
+w2 = Work.create(title: 'rusrails.ru', description: 'Командная строка Rails. После прочтения этого руководства, вы узнаете: Как создать приложение на Rails. Как генерировать модели, контроллеры, миграции базы данных и юнит-тесты. Как запустить сервер для разработки. Как экспериментировать с объектами в интерактивной оболочке', language: 'Ruby on Rails', price: 6000)
+w3 = Work.create(title: 'Portfolio', description: 'Mu first project.', language: 'Ruby on Rails', price: 1)
