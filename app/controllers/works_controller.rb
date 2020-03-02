79e5e6b8f303122bd5ce1b25dbadc7fc70d55b04
bogomol_sergo <breinkiller650@gmail.com>
2020-03-02 12:45:13 +0200

    Editing and deleting users and works from ADM-page. (complete\work)

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,7 @@
 class WorksController < ApplicationController
   before_action :require_user, only: :show
   before_action :require_editor, only: %i[update edit]
+  before_action :require_admin, only: %i[edit update destroy]
 
   def index
     @works = Work.all
@@ -10,9 +11,29 @@ class WorksController < ApplicationController
     @work = Work.find(params[:id])
   end
 
-  def edit; end
+  def edit
+    flash[:success] = 'Project updated!'
+    @work = Work.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @work = Work.find(params[:id])
+    if @work.update_attributes(work_params)
+      redirect_to work_path
+    else
+      redirect_to admin_path
+    end
+  end
 
-  def destroy; end
+  def destroy
+    flash[:success] = 'Project removed!'
+    Work.find(params[:id]).destroy
+    redirect_to admin_path
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :description, :language, :price)
+  end
 end
